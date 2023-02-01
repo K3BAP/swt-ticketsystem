fb80e3c05a4d497d684278e0e15f27dd4184a865
Fabian Sponholz <s4faspon@uni-trier.de>
2023-02-01 19:11:09 +0100

    Tarifübersichts-Seite für Admin in Grundzügen erstellt

---
+++ vue/swt-tickets/src/components/TarifuebersichtTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Preis Preisstufe 1
+        </th>
+        <th class="text-left">
+          Preis Preisstufe 2
+        </th>
+        <th class="text-left">
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="item in tarife"
+        :key="item.name"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.preis1 }}</td>
+        <td>{{ item.preis2 }}</td>
+        <td class="text-right">
+          <v-btn
+            color="primary"
+            class="ma-2 white--text"
+            small
+            fab
+          >
+            <v-icon dark>
+              mdi-pencil
+            </v-icon>
+          </v-btn>
+          <v-btn
+            color="red"
+            class="ma-2 white--text"
+            small
+            fab
+          >
+            <v-icon dark>
+              mdi-trash-can
+            </v-icon>
+          </v-btn>
+        </td>
+      </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: 'TarifuebersichtTable',
+  data () {
+    return {
+      tarife: [
+        {
+          name: 'Einzelticket',
+          preis1: '2,20 €',
+          preis2: '3,10 €'
+        },
+        {
+          name: '4-Fahrten-Ticket',
+          preis1: '6,60 €',
+          preis2: '9,00 €'
+        },
+        {
+          name: 'Tagesticket',
+          preis1: '6,10 €',
+          preis2: '8,00 €'
+        },
+        {
+          name: 'Wochenticket',
+          preis1: '20,00 €',
+          preis2: '25,40 €'
+        },
+        {
+          name: 'Monatsticket',
+          preis1: '70,20 €',
+          preis2: '88,20 €'
+        }
+      ]
+    }
+  }
+}
+</script>
--- vue/swt-tickets/src/views/TarifuebersichtView.vue
+++ vue/swt-tickets/src/views/TarifuebersichtView.vue
@@ -1,10 +1,43 @@
 <template>
-<h1> Tarifübersicht </h1>
+  <v-container>
+  <v-row justify="center">
+    <v-col cols="12" md="9">
+      <v-card
+        :elevation="15">
+        <v-container>
+          <v-row class="pa-3" justify="end">
+            <v-col cols="12">
+              <h1> Tarifübersicht </h1>
+            </v-col>
+            <v-col cols="12" >
+              <tarifuebersicht-table />
+            </v-col>
+            <v-btn
+              :loading="loading5"
+              :disabled="loading5"
+              color="blue-grey"
+              class="ma-2 white--text"
+              fab
+              @click="loader = 'loading5'"
+            >
+              <v-icon dark>
+                mdi-plus
+              </v-icon>
+            </v-btn>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-col>
+  </v-row>
+  </v-container>
 </template>
 
 <script>
+import TarifuebersichtTable from '@/components/TarifuebersichtTable.vue'
+
 export default {
-  name: 'TarifuebersichtView'
+  name: 'TarifuebersichtView',
+  components: { TarifuebersichtTable }
 }
 </script>
 
